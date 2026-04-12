74f31402d48d7084b6768412c8c6906e5cd40368
Mdjakaria53 <kjakaria53@gmail.com>
2026-04-12 07:10:01 +0300

    first commit

---
+++ into/into.py
@@ -0,0 +1,21 @@
+
+
+n = 1
+while n<=5:
+    print("jakaria")
+    n+=1
+
+print(100//3)
+
+
+if 1<5:
+    print("yes")
+elif 2<9:
+    print("no")
+else:
+    print("no")
+
+
+num = [2,23,43,23,212,32]
+
+
